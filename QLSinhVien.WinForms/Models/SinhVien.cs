9db2e5be2f9e409837dddeb679531a3906147ae0
nhi <ngocnhihophuong2599@gmail.com>
2026-08-22 14:32:40 +0700

    Add project files.

---
+++ QLSinhVien.WinForms/Models/SinhVien.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace QLSinhVien.WinForms.Models
+{
+    public class SinhVien
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        
+        [BsonElement("masv")]
+        public string Masv { get; set; } = string.Empty;
+
+        [BsonElement("hoten")]
+        public string Hoten { get; set; } = string.Empty;
+
+        [BsonElement("tuoi")]
+        public int Tuoi { get; set; }
+
+        [BsonElement("phai")]
+        public string Phai { get; set; } = string.Empty;  
+
+        [BsonElement("malop")]
+        public string Malop { get; set; } = string.Empty;
+
+        [BsonElement("ngoaingu")]
+        public List<string> Ngoaingu { get; set; } = new();
+        
+        [BsonElement("monhoc")]
+        public List<MonHoc> Monhoc { get; set; } = new();
+    }
+}
+
---
+++ QLSinhVien.WinForms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace QLSinhVien.WinForms
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+
+            // --- TEST KẾT NỐI MONGODB ---
+            try
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+                IConfigurationRoot config = builder.Build();
+                string connectionString = config["MongoDB:ConnectionString"];
+                var client = new MongoClient(connectionString);
+                client.ListDatabaseNames().ToList();
+                MessageBox.Show("Kết nối MongoDB thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối: \n" + ex.Message, "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            Application.Run(new Form1());
+        }
+    }
+}
